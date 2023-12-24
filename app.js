4d33af70d675d0683aa9911bf448d16e86ffbb52
maxim <95708730+Wilbemax@users.noreply.github.com>
2023-12-24 15:32:57 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const WIDHT = 600;
+const PADDING = 40;
+const HEIGHT = 200;
+const DPI_WIDTH = WIDHT * 2;
+const DPI_HEIGHT = HEIGHT * 2;
+const VIEW_HEIGHT = DPI_HEIGHT - PADDING * 2;
+const ROWS_COUNT = 5;
+
+function chart(canvas, data) {
+	const ctx = canvas.getContext('2d');
+	canvas.style.width = WIDHT + 'px';
+	canvas.style.height = HEIGHT + 'px';
+	canvas.width = DPI_WIDTH;
+	canvas.height = DPI_HEIGHT;
+
+	//==== y axis\
+
+	const step = VIEW_HEIGHT / ROWS_COUNT;
+
+	ctx.beginPath();
+	ctx.strokeStyle = '#bbb';
+	ctx.font = 'normal 20px Helvetica, sans-serif';
+	ctx.fillStyle = '#96a2aa';
+
+	for (let i = 1; i <= ROWS_COUNT; i++) {
+		const y = step * i;
+		ctx.fillText(DPI_HEIGHT - y, 5, y - 10);
+		ctx.moveTo(0, y + PADDING);
+		ctx.lineTo(DPI_WIDTH, y + PADDING);
+	}
+	ctx.stroke();
+	ctx.closePath();
+	//====
+
+	ctx.beginPath();
+	ctx.lineWidth = 4;
+	ctx.strokeStyle = 'green';
+	for (const [x, y] of data) {
+		ctx.lineTo(x, DPI_HEIGHT - y);
+	}
+	ctx.stroke();
+	ctx.closePath();
+}
+
+chart(document.getElementById('chart'), [
+	[0, 0],
+	[200, 200],
+	[400, 100],
+	[600, 300],
+	[800, 50],
+	[1000, 350],
+]);
